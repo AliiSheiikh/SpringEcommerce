9e436099a404abc0933fea2e7d227a90c4b161e8
Ali Sheikh <l_lawliet786@hotmail.com>
2025-01-28 23:48:31 -0500

    added validation errors

--- src/main/java/com/Ali/EcommerceApp/controller/ProductController.java
+++ src/main/java/com/Ali/EcommerceApp/controller/ProductController.java
@@ -3,11 +3,16 @@ package com.Ali.EcommerceApp.controller;
 import com.Ali.EcommerceApp.models.Product;
 import com.Ali.EcommerceApp.models.ProductDTO;
 import com.Ali.EcommerceApp.repo.ProductRepo;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,5 +37,18 @@ public class ProductController {
         ProductDTO productDTO = new ProductDTO();
         model.addAttribute("productDTO", productDTO);
         return "products/CreateProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@Valid @ModelAttribute ProductDTO productDTO, BindingResult result){
+
+        if (productDTO.getImageFile().isEmpty()){
+            result.addError(new FieldError("productDTO","imageFile","The image file is required"));
+        }
+
+        if (result.hasErrors()){
+            return "products/CreateProduct";
+        }
+        return "redirect:/products";
     }
 }
