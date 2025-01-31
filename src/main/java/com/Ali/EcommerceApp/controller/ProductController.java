041e21bbc8cc0f9ea9d30566c20fa6784be05588
Ali Sheikh <l_lawliet786@hotmail.com>
2025-01-30 23:21:36 -0500

    fixed price not being saved in database

--- src/main/java/com/Ali/EcommerceApp/controller/ProductController.java
+++ src/main/java/com/Ali/EcommerceApp/controller/ProductController.java
@@ -113,4 +113,53 @@ public class ProductController {
         }
         return "products/EditProduct";
     }
+
+    @PostMapping("/edit")
+    public String updateProduct(Model model, @RequestParam int id, @Valid @ModelAttribute ProductDTO productDTO,
+                                BindingResult result){
+        try{
+            Product product = repo.findById(id).get();
+            model.addAttribute("product, product");
+
+            if (result.hasErrors()){
+                return "products/EditProduct";
+            }
+
+            if (!productDTO.getImageFile().isEmpty()){   //delete old image
+                String uploadDir = "public/images/";
+                Path oldImagePath = Paths.get(uploadDir+product.getImageFileName());
+
+                try{
+                    Files.delete(oldImagePath);
+                }
+                catch(Exception ex){
+                    System.out.println("Exception: "+ ex.getMessage());
+                }
+
+                //save new image file
+                MultipartFile image = productDTO.getImageFile();
+                Date createdAt = new Date();
+                String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+                try (InputStream inputStream = image.getInputStream()){
+                    Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                            StandardCopyOption.REPLACE_EXISTING);
+                }
+                product.setImageFileName((storageFileName));
+            }
+
+            product.setName(productDTO.getName());
+            product.setBrand(productDTO.getBrand());
+            product.setCategory(productDTO.getCategory());
+            product.setPrice(productDTO.getPrice());
+            product.setDescription(productDTO.getDescription());
+
+            repo.save(product);
+
+        }
+        catch (Exception ex){
+            System.out.println("Exception "+ex.getMessage());
+        }
+        return "redirect:/products";
+    }
 }
